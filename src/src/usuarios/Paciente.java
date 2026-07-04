56d30a045427e19290541a0411c2f99000fe6fa7
Isaac <isaacsebastian144@gmail.com>
2026-07-03 19:57:44 -0500

    Este commit es para definir los atributos básicos y médicos de la clase Paciente

---
+++ src/src/usuarios/Paciente.java
@@ -0,0 +1,18 @@
+package usuarios;
+
+public class Paciente {
+    // Atributos privados básicos
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String telefono;
+    private String correo;
+
+    // Atributos privados médicos
+    private String tipoSangre;
+    private String alergias;
+    private double peso;
+    private double altura;
+    private String medicacion;
+
+}
